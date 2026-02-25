b45a94e5f2a0ec87223f11d8cac3ab1cb09ae7a6
Torsten Curdt <tcurdt@vafer.org>
2026-02-25 04:10:14 +0100

    boot mount

--- dd-nixos/disco-efi.nix
+++ dd-nixos/disco-efi.nix
@@ -14,7 +14,7 @@
             content = {
               type = "filesystem";
               format = "vfat";
-              mountpoint = "/boot/efi";
+              mountpoint = "/boot";
             };
           };
           root = {
